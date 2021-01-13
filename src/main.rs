533e339a59f70d29873cf91910e28932f2c7fab6
MoAlyousef <may642_2000@hotmail.com>
2021-01-13 16:09:52 +0300

    first commit

---
+++ src/main.rs
@@ -0,0 +1,12 @@
+use fltk::*;
+mod mainview;
+
+fn main() {
+    let app = app::App::default().with_scheme(app::Scheme::Gtk);
+    let mut ui = mainview::UserInterface::make_window();
+    let mut frame = ui.frame.clone();
+    ui.but.set_callback(move || {
+        frame.set_label("Hello World!");
+    });
+    app.run().unwrap();
+}
