64462ebfcbf008b4fbbcd4d1be01ba7132939f60
Mohammed Alyousef <may642_2000@hotmail.com>
2021-04-19 15:17:46 +0300

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
-use fltk::*;
+use fltk::{prelude::*, *};
 mod mainview;
 
 fn main() {
     let app = app::App::default().with_scheme(app::Scheme::Gtk);
     let mut ui = mainview::UserInterface::make_window();
     let mut frame = ui.frame.clone();
-    ui.but.set_callback(move || {
+    ui.but.set_callback(move |_| {
         frame.set_label("Hello World!");
     });
     app.run().unwrap();
