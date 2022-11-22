57cd97be8287f957b37f5ec31ddba218361dc3c0
Mohammed Alyousef <may642_2000@hotmail.com>
2022-11-22 10:33:44 +0300

    Delete build.rs

--- build.rs
+++
@@ -1,9 +0,0 @@
-use std::path::PathBuf;
-use std::env;
-
-fn main() {
-    println!("cargo:rerun-if-changed=gui/mainview.fl");
-    let g = fl2rust::Generator::default();
-    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
-    g.in_out("gui/mainview.fl", out_path.join("mainview.rs").to_str().unwrap()).expect("Failed to generate rust from fl file!");
-}
